1b59cdf7109789cd6d519b951ac74684ff968215
gabrieudev <joao.gabriel@atoscapital.com.br>
2025-10-31 20:02:52 -0300

    ✨ feat: criar script k6

---
+++ k6/load.js
@@ -0,0 +1,18 @@
+import http from "k6/http";
+import { sleep } from "k6";
+
+export let options = {
+    vus: __ENV.VUS ? parseInt(__ENV.VUS) : 100,
+    duration: __ENV.DURATION ? __ENV.DURATION : "30m",
+    thresholds: {
+        http_req_duration: ["p(95)<2000"],
+    },
+};
+
+const BASE = __ENV.TARGET_URL || "http://host.docker.internal:3000";
+
+export default function () {
+    http.get(`${BASE}/ping`);
+    
+    sleep(0.05);
+}
